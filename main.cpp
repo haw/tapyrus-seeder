3157a54a9df983fe5605ba452dd63aeb8e6e63da
Yukishige Nakajo <nakajo@haw.co.jp>
2018-11-06 15:26:52 +0900

    change: Change message header bytes and initial seeds.

--- main.cpp
+++ main.cpp
@@ -397,12 +397,8 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"dnsseed.bluematt.me", "bitseed.xf2.org", "dnsseed.bitcoin.dashjr.org", "seed.bitcoin.sipa.be", ""};
-static const string testnet_seeds[] = {"testnet-seed.alexykot.me",
-                                       "testnet-seed.bitcoin.petertodd.org",
-                                       "testnet-seed.bluematt.me",
-                                       "testnet-seed.bitcoin.schildbach.de",
-                                       ""};
+static const string mainnet_seeds[] = {"seed.tapyrus.dev.haw.biz"};
+static const string testnet_seeds[] = {"seed.tapyrus.dev.haw.biz"};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
@@ -459,10 +455,11 @@ int main(int argc, char **argv) {
   bool fDNS = true;
   if (opts.fUseTestNet) {
       printf("Using testnet.\n");
-      pchMessageStart[0] = 0x0b;
-      pchMessageStart[1] = 0x11;
-      pchMessageStart[2] = 0x09;
-      pchMessageStart[3] = 0x07;
+      // 0x75 0x9A 0x83 0x74
+      pchMessageStart[0] = 0x74;
+      pchMessageStart[1] = 0x9a;
+      pchMessageStart[2] = 0x83;
+      pchMessageStart[3] = 0x74;
       seeds = testnet_seeds;
       fTestNet = true;
   }
--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,8 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+// 01FFF000
+unsigned char pchMessageStart[4] = { 0x01, 0xff, 0xf0, 0x00 };
 
 CMessageHeader::CMessageHeader()
 {
